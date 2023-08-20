bd778f34d375b98d6b9f71e598bc9869b4d5024b
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-08-20 22:20:44 +0200

    Operation 24 started
    Co-authored-by: DavideBlast <DavideBlast@users.noreply.github.com>
    Co-authored-by: Pietro Olivi <pietroolivi@users.noreply.github.com>

--- src/main/java/application/operations/OpFactory.java
+++ src/main/java/application/operations/OpFactory.java
@@ -37,12 +37,112 @@ public class OpFactory {
             }
         };
     }
+
+    public Operation createOp24() {
+        return new Operation() {
+
+            @Override
+            public String getQuery(Optional<List<Object>> args) {
+
+                return "SET @newHashAmbiente = floor(in_pm25media*10)*1001 + floor(in_percentualeSpazioVerdeUrbano*10);" + 
+                       "SET @newHashEconomia = floor((in_tassoDisoccupazione * 10)) * 7239535 + floor((in_PILProCapite / 1000)) * 8035 + floor((in_stipendioMedio - 300));" + 
+                       "SET @newHashIstruzione = floor((in_numeroUniversita * 10)) * 1002001 + floor((in_percentualeDiplomati * 10)) * 1001 + floor((in_percentualeLaureati * 10));" +
+                       "SET @newHashSanita = floor((in_postiLettoProCapite * 10)) * 501 + floor((in_aspettativaVita * 10 - 500));" +  
+                       "SET @newHashTrasporto = floor((in_autoProCapite * 100)) * 151 + in_percorrenzaMediaPendolare;" + 
+                       "IF NOT EXISTS (" + 
+                       "SELECT 1" + 
+                       "FROM Ambiente" + 
+                       "WHERE hashAmbiente = @newHashAmbiente" + 
+                       ") THEN" + 
+                       "INSERT INTO Ambiente (hashAmbiente, pm25media, percentualeSpazioVerdeUrbano)" + 
+                       "VALUES (@newHashAmbiente, in_pm25media, in_percentualeSpazioVerdeUrbano);" + 
+                       "END IF;" + 
+                       "IF NOT EXISTS (" + 
+                       "SELECT 1" + 
+                       "FROM Economia" + 
+                       "WHERE hashEconomia = @newHashEconomia" + 
+                       ") THEN" + 
+                       "INSERT INTO Economia (hashEconomia, tassoDisoccupazione, PILProCapite, stipendioMedio)" + 
+                       "VALUES (@newHashEconomia, in_tassoDisoccupazione, in_PILProCapite, in_stipendioMedio);" + 
+                       "END IF;" + 
+                       "IF NOT EXISTS (" + 
+                       "SELECT 1" + 
+                       "FROM Istruzione" + 
+                       "WHERE hashIstruzione = @newHashIstruzione" + 
+                       ") THEN" + 
+                       "INSERT INTO Istruzione (hashIstruzione, percentualeLaureati, percentualeDiplomati, numeroUniversita)" + 
+                       "VALUES (@newHashIstruzione, in_percentualeLaureati, in_percentualeDiplomati, in_numeroUniversita);" + 
+                       "END IF;" + 
+                       "IF NOT EXISTS (" + 
+                       "SELECT 1" + 
+                       "FROM Sanita" + 
+                       "WHERE hashSanita = @newHashSanita" + 
+                       ") THEN" + 
+                       "INSERT INTO Sanita (hashSanita, postiLettoProCapite, aspettativaVita)" + 
+                       "VALUES (@newHashSanita, in_postiLettoProCapite, in_aspettativaVita);" + 
+                       "END IF;" + 
+                       "IF NOT EXISTS (" + 
+                       "SELECT 1" + 
+                       "FROM Trasporto" + 
+                       "WHERE hashTrasporto = @newHashTrasporto" + 
+                       ") THEN" + 
+                       "INSERT INTO Trasporto (hashTrasporto, percorrenzaMediaPendolare, autoProCapite)" + 
+                       "VALUES (@newHashTrasporto, in_percorrenzaMediaPendolare, in_autoProCapite);" + 
+                       "END IF;" + 
+                       "SET @punteggioAmbiente = " + ((10 - (Float) args.get().get(2) / 5) + (Float) args.get().get(3) > 30 ? 10 : (Float) args.get().get(3) / 3) / 2 + 
+                       "SET @punteggioEconomia =  " + 
+                       "SET @punteggioIstruzione = " +
+                       "SET @punteggioSanita = " +
+                       "SET @punteggioTrasporto = " +
+                       "INSERT INTO citta_anni" + 
+                       "(idCitta, anno, punteggioAmbiente, punteggioTrasporto, punteggioEconomia, punteggioSanita, punteggioIstruzione, hashAmbiente, hashEconomia, hashIstruzione, hashSanita, hashTrasporto) " + 
+                       "VALUES (,,,,,,,,,,,)";
+            }
+
+            @Override
+            public List<String> getInputNames() {
+                return List.of("idCitta",
+                               "anno",
+                               "pm25media",
+                               "percentualeSpazioVerdeUrbano",
+                               "pilprocapite",
+                               "stipendioMedio",
+                               "tassoDisoccupazione",
+                               "percentualeLaureati",
+                               "percentualeDiplomati",
+                               "numeroUniversita",
+                               "aspettativaVita",
+                               "postiLettoProCapite",
+                               "percorrenzaMediaPendolare",
+                               "autoProCapite");
+            }
+
+            @Override
+            public List<Tipo> getInputTypes() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getInputTypes'");
+            }
+
+            @Override
+            public List<Object> translateInput(List<String> inputs) {
+                return List.of(Integer.parseInt(inputs.get(0)),     //idCitta
+                               Integer.parseInt(inputs.get(1)),    //anno
+                               Float.parseFloat(inputs.get(2)),    //pm25media
+                               Float.parseFloat(inputs.get(3)),    //percentualeSpazioVerdeUrbano
+                               Float.parseFloat(inputs.get(4)),    //pilprocapite
+                               Integer.parseInt(inputs.get(5)),    //stipendioMedio
+                               Float.parseFloat(inputs.get(6)),    //tassoDisoccupazione
+                               Float.parseFloat(inputs.get(7)),    //percentualeLaureati
+                               Float.parseFloat(inputs.get(8)),    //percentualeDiplomati
+                               Integer.parseInt(inputs.get(9)),    //numeroUniversita
+                               Float.parseFloat(inputs.get(10)),   //aspettativaVita
+                               Float.parseFloat(inputs.get(11)),   //postiLettoProCapite
+                               Float.parseFloat(inputs.get(12)),   //percorrenzaMediaPendolare
+                               Float.parseFloat(inputs.get(13)));  //autoProCapite
+            }
+            
+        };
+    }
     
 }
-
-
-// SELECT I.*
-// FROM immobili I JOIN annunci_utente A ON (I.idImmobile = A.idImmobile
-// AND A.statoAnnuncio = 'Attivo' AND I.idZona = ?)
-// ORDER BY dataCreazione DESC
 
